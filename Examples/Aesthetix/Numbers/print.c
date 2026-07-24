528b4a1a944bfdff2c3d457eeb3b225d772789df
Sergio Mendieta <79947025+Surg-io@users.noreply.github.com>
2026-07-23 18:33:00 -0700

    Update print.c

--- Examples/Aesthetix/Numbers/print.c
+++ Examples/Aesthetix/Numbers/print.c
@@ -1,4 +1,20 @@
- while (1)
+int (*getArray(int currnum))[13] {
+    switch (currnum) {
+        case 0: return zero;
+        case 1: return one;
+        case 2: return two;
+        case 3: return three;
+        case 4: return four;
+        case 5: return five;
+        case 6: return six;
+        case 7: return seven;
+        case 8: return eight;
+        default: return nine;
+    };
+};
+
+
+while (1)
     {
         if (currnum > 9){ //Two digit print
             tens = getArray(currnum / 10);
